7f7fbedac786826c3971decbffe97e6799fbc698
Yury Bolbas <yury.bolbas@gmail.com>
2018-11-14 15:42:55 +0300

    Moved getSourceUrl function to modules, updated 'for' cycles.

--- modules.js
+++ modules.js
@@ -53,3 +53,9 @@ export const toggleExpander = () => {
 		sideBlock.setAttribute('class', 'collapsed')
 	}
 };
+
+export const getSourceUrl = (sourceId) => {
+	return `https://newsapi.org/v2/top-headlines?sources= +
+			${sourceId} +
+			&apiKey=2b17f156630a4c0caf074c1251e75c02`;
+};
