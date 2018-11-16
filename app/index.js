2365acaaf8f507e5dbaa8bb5bc5cc9a9090e4127
Yury Bolbas <yury.bolbas@gmail.com>
2018-11-16 18:37:18 +0300

    Added Babel.

---
+++ app/index.js
@@ -0,0 +1,140 @@
+'use strict';
+
+var renderArticles = function renderArticles(promiseValue) {
+  if (promiseValue && promiseValue.articles) {
+    var articlesList = "";
+    var articleNumber = getArticleNumber();
+
+    if (promiseValue.articles.length > 0 && promiseValue.articles[0].source) {
+      var sourceTitle = promiseValue.articles[0].source.name;
+      console.debug("Source Title to Show: " + sourceTitle);
+      document.getElementById('source-title').innerHTML = sourceTitle;
+    }
+
+    var length = promiseValue.articles.length > articleNumber ? articleNumber : promiseValue.articles.length;
+    console.debug("Article Number to Show: " + length);
+
+    for (var i = 0; i < length; i++) {
+      var article = promiseValue.articles[i];
+      articlesList += "<div class=\"article\">\n\t\t\t\t\t\t\t<h2><a href=\"".concat(article.url, "\" target=\"_blank\">").concat(article.title, "</a></h2>");
+
+      if (article.publishedAt) {
+        var date = new Date(Date.parse(article.publishedAt));
+        articlesList += "<div class=\"date\">Published: ".concat(date, "</div>");
+      }
+
+      if (article.urlToImage) {
+        articlesList += "<img src=\"".concat(article.urlToImage, "\" alt=\"\">");
+      }
+
+      if (article.content) {
+        articlesList += "<p>".concat(article.content, "</p>");
+      } else if (article.description) {
+        articlesList += "<p>".concat(article.description, "</p>");
+      }
+
+      articlesList += "</div>";
+    }
+
+    document.getElementById('articles-list').innerHTML = articlesList;
+  }
+};
+
+var getArticleNumber = function getArticleNumber() {
+  var selector = document.getElementById('articles-number');
+  var value = selector[selector.selectedIndex].value;
+  console.debug("Selected Article Number: " + value);
+  return value;
+};
+
+var toggleExpander = function toggleExpander() {
+  var sideBlock = document.getElementById('side-block');
+  var sideBlockClass = sideBlock.getAttribute('class');
+
+  if (sideBlockClass === 'collapsed') {
+    sideBlock.removeAttribute('class');
+  } else {
+    sideBlock.setAttribute('class', 'collapsed');
+  }
+};
+
+var getSourceUrl = function getSourceUrl(sourceId) {
+  return "https://newsapi.org/v2/top-headlines?sources= +\n\t\t\t".concat(sourceId, " +\n\t\t\t&apiKey=2b17f156630a4c0caf074c1251e75c02");
+};
+
+var currentSourceId = '';
+
+var onSourceClick = function onSourceClick(event) {
+  console.log("Old SourceId: ".concat(currentSourceId));
+
+  if (event.target.id !== 'sources-list') {
+    currentSourceId = event.target.id;
+  }
+
+  console.log("New SourceId: ".concat(currentSourceId));
+  loadSource();
+  toggleExpander();
+};
+
+var loadSource = function loadSource() {
+  if (!currentSourceId) {
+    return;
+  }
+
+  console.debug("loadSource() called");
+  console.debug("Selected Source Id: " + currentSourceId);
+  var sourceUrl = getSourceUrl(currentSourceId);
+  var req = new Request(sourceUrl);
+  fetch(req).then(function (response) {
+    var articlesResult = response.json();
+    articlesResult.then(renderArticles);
+  }).catch(function () {
+    return console.error("Response Error from: ".concat(currentSourceId));
+  });
+};
+
+document.getElementById('articles-number').addEventListener('change', loadSource);
+var sourcesUrl = "https://newsapi.org/v2/sources?apiKey=2b17f156630a4c0caf074c1251e75c02";
+var sourcesReq = new Request(sourcesUrl);
+fetch(sourcesReq).then(function (sourcesResponse) {
+  var result = sourcesResponse.json();
+  console.log(result);
+  result.then(function (promiseValue) {
+    if (promiseValue && promiseValue.sources && promiseValue.sources.length > 0) {
+      var list = "";
+      var _iteratorNormalCompletion = true;
+      var _didIteratorError = false;
+      var _iteratorError = undefined;
+
+      try {
+        for (var _iterator = promiseValue.sources[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+          var source = _step.value;
+          list += "<li id= ".concat(source.id, "  >  ").concat(source.name, "  </li>");
+        }
+      } catch (err) {
+        _didIteratorError = true;
+        _iteratorError = err;
+      } finally {
+        try {
+          if (!_iteratorNormalCompletion && _iterator.return != null) {
+            _iterator.return();
+          }
+        } finally {
+          if (_didIteratorError) {
+            throw _iteratorError;
+          }
+        }
+      }
+
+      document.getElementById('sources-list').innerHTML += list;
+      currentSourceId = promiseValue.sources[0].id;
+      loadSource();
+      var items = document.getElementById('sources-list').getElementsByTagName('li');
+      var itemsList = document.getElementById('sources-list');
+      itemsList.addEventListener("click", onSourceClick);
+    }
+  });
+}).catch(function () {
+  return console.error("Response Error from: ".concat(sourcesUrl));
+});
+document.getElementById('expander').addEventListener("click", toggleExpander);
---
+++ src/index.js
@@ -0,0 +1,123 @@
+'use strict';
+
+let renderArticles = (promiseValue) => {
+	if (promiseValue && promiseValue.articles) {
+		let articlesList = "";
+		let articleNumber = getArticleNumber();
+
+		if (promiseValue.articles.length > 0 && promiseValue.articles[0].source) {
+			let sourceTitle = promiseValue.articles[0].source.name;
+			console.debug("Source Title to Show: " + sourceTitle);
+			document.getElementById('source-title').innerHTML = sourceTitle;
+		}
+
+		let length = promiseValue.articles.length > articleNumber ? articleNumber : promiseValue.articles.length;
+		console.debug("Article Number to Show: " + length);
+
+		for (let i = 0; i < length; i++) {
+			let article = promiseValue.articles[i];
+			articlesList += `<div class="article">
+							<h2><a href="${article.url}" target="_blank">${article.title}</a></h2>`;
+			if (article.publishedAt) {
+				let date = new Date(Date.parse(article.publishedAt));
+				articlesList += `<div class="date">Published: ${date}</div>`;
+			}
+			if (article.urlToImage) {
+				articlesList += `<img src="${article.urlToImage}" alt="">`;
+			}
+			if (article.content) {
+				articlesList += `<p>${article.content}</p>`;
+			} else if (article.description) {
+				articlesList += `<p>${article.description}</p>`;
+			}
+			articlesList += `</div>`;
+		}
+
+		document.getElementById('articles-list').innerHTML = articlesList;
+	}
+};
+
+let getArticleNumber = () => {
+	let selector = document.getElementById('articles-number');
+	let value = selector[selector.selectedIndex].value;
+	console.debug("Selected Article Number: " + value);
+	return value;
+};
+
+const toggleExpander = () => {
+	let sideBlock = document.getElementById('side-block');
+	let sideBlockClass = sideBlock.getAttribute('class');
+	if (sideBlockClass === 'collapsed') {
+		sideBlock.removeAttribute('class')
+	} else {
+		sideBlock.setAttribute('class', 'collapsed')
+	}
+};
+
+const getSourceUrl = (sourceId) => {
+	return `https://newsapi.org/v2/top-headlines?sources= +
+			${sourceId} +
+			&apiKey=2b17f156630a4c0caf074c1251e75c02`;
+};
+
+let currentSourceId = '';
+
+let onSourceClick = (event) => {
+	console.log(`Old SourceId: ${currentSourceId}`);
+	if (event.target.id !== 'sources-list') {
+		currentSourceId = event.target.id;
+	}
+	console.log(`New SourceId: ${currentSourceId}`);
+	loadSource();
+	toggleExpander();
+};
+
+let loadSource = () => {
+	if (!currentSourceId) {
+		return;
+	}
+
+	console.debug("loadSource() called");
+	console.debug("Selected Source Id: " + currentSourceId);
+
+	let sourceUrl = getSourceUrl(currentSourceId);
+	let req = new Request(sourceUrl);
+	fetch(req)
+		.then((response) => {
+			let articlesResult = response.json();
+			articlesResult.then(renderArticles);
+		})
+		.catch(() => console.error(`Response Error from: ${currentSourceId}`));
+};
+
+document.getElementById('articles-number').addEventListener('change', loadSource);
+
+const sourcesUrl = "https://newsapi.org/v2/sources?apiKey=2b17f156630a4c0caf074c1251e75c02";
+const sourcesReq = new Request(sourcesUrl);
+
+fetch(sourcesReq)
+	.then((sourcesResponse) => {
+		let result = sourcesResponse.json();
+		console.log(result);
+		result.then((promiseValue) => {
+			if (promiseValue && promiseValue.sources && promiseValue.sources.length > 0) {
+				let list = "";
+				for (let source of promiseValue.sources) {
+					list += `<li id= ${source.id}  >  ${source.name}  </li>`;
+				}
+
+				document.getElementById('sources-list').innerHTML += list;
+
+				currentSourceId = promiseValue.sources[0].id;
+				loadSource();
+
+				let items = document.getElementById('sources-list').getElementsByTagName('li');
+				let itemsList = document.getElementById('sources-list');
+				itemsList.addEventListener("click", onSourceClick);
+
+			}
+		});
+	})
+	.catch(() => console.error(`Response Error from: ${sourcesUrl}`));
+
+document.getElementById('expander').addEventListener("click", toggleExpander);
