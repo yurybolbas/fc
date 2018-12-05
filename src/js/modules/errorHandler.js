24bc35b24d7c45510ea05ef0820957cd893d985b
Yury Bolbas <yury.bolbas@gmail.com>
2018-12-05 13:18:41 +0300

    Updated logic to add new errors to the existing error modal.

--- src/js/modules/errorHandler.js
+++ src/js/modules/errorHandler.js
@@ -5,11 +5,20 @@ export const Modal = (() => {
 		divError;
 
 	let getModal = (errText) => {
-		divError = document.createElement('div');
-		divError.id = 'error-popup';
-		divError.innerHTML = `<div class="error-body"><div class="error-content"><p>${errText}</p>
-		<button id="err-close">Close</button></div></div>`;
-		document.body.appendChild(divError);
+		let errItem = `<li>${errText}</li>`;
+		if (document.getElementById('error-popup')) {
+			divError = document.getElementById('error-popup');
+			let errList = document.getElementById('errors-list');
+			errList.innerHTML += errItem;
+		} else {
+			divError = document.createElement('div');
+			divError.id = 'error-popup';
+			divError.innerHTML = `<div class="error-body"><div class="error-content">
+				<h2>Error</h2>
+				<ol id="errors-list">${errItem}</ol>
+				<button id="err-close">Close</button></div></div>`;
+			document.body.appendChild(divError);
+		}
 		document.getElementById('err-close').addEventListener("click", () => elRemove(divError.id));
 		return divError;
 	};
